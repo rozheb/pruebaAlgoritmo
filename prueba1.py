8144eae8463ce0d24e2d59fcdb77c0f920086d1d
rozheb <rozheb20001@gmail.com>
2024-05-31 01:32:44 -0500

    upd

--- prueba1.py
+++ prueba1.py
@@ -28,11 +28,41 @@ import matplotlib.pyplot as plt
 import seaborn as sb
 
 ## cargar datos
-datos = pd.read_csv("https://raw.githubusercontent.com/rozheb/pruebaAlgoritmo/main/datasetrutas.csv")
+datos = pd.read_csv("https://raw.githubusercontent.com/rozheb/pruebaAlgoritmo/main/datasetrutas.csv", on_bad_lines='skip')
 datos
 
 print("Observaciones y variables: ", datos.shape)
-print("Columnas y tipo de dato")
-datos.columns
+
+## Columnas y tipo de dato
 datos.dtypes
 
+#carga de datos 
+X = datos.drop(columns='xi')
+y = datos['xi']
+# Identificar las columnas categóricas
+columnas_categoricas = X.select_dtypes(include=['object']).columns
+
+# Crear el transformador para las variables categóricas
+preprocessor = ColumnTransformer(
+    transformers=[
+        ('cat', OneHotEncoder(), columnas_categoricas)
+    ],
+    remainder='passthrough'  # Mantener las columnas numéricas tal como están
+)
+
+# Dividir los datos en entrenamiento y validación
+X_entrena, X_valida, Y_entrena, Y_valida = train_test_split(X, y, train_size=0.80, random_state=1280)
+
+# Aplicar el transformador a los datos de entrenamiento y validación
+X_entrena = preprocessor.fit_transform(X_entrena)
+X_valida = preprocessor.transform(X_valida)
+
+# Crear y entrenar el modelo KNN
+knn = KNeighborsClassifier(n_neighbors=12)
+knn.fit(X_entrena, Y_entrena)
+
+# Evaluar el modelo
+y_pred = knn.predict(X_valida)
+print("Accuracy:", accuracy_score(Y_valida, y_pred))
+print("Confusion Matrix:\n", confusion_matrix(Y_valida, y_pred))
+print("Classification Report:\n", classification_report(Y_valida, y_pred))
